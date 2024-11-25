147c5eaa98c3cb410c94533c728a22be1b682c04
Harsh Goyal <hgoyal@cosmoprof.com>
2024-11-25 23:04:57 +0530

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1795706931"
+    serviceWorkerVersion: "3925912103"
   }
 });
